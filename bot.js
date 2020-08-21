d1d546734d29237c964f6f41b02ebb0bcbedcf81
TBuHHu <tbuhhu@protonmail.com>
2020-08-21 19:57:45 +0300

    refactoring for mongodb is finished... beta version is born!

--- bot.js
+++ bot.js
@@ -1,23 +1,23 @@
-require('dotenv').config()
-const { MongoClient } = require("mongodb");
-const { Telegraf, Telegram } = require('telegraf');
+require('dotenv').config();
+const { MongoClient } = require('mongodb');
+const { Telegraf } = require('telegraf');
 const request = require('request');
 const cron = require('node-cron');
-var fs = require('fs');
 
 const bot = new Telegraf(process.env.BOT_TOKEN);
 const uri = process.env.MONGO_DB;
 const client = new MongoClient(uri);
+
 client.connect();
 
-var usdValue;
-var eurValue;
+let usdValue;
+let eurValue;
 
-request('https://www.cbr-xml-daily.ru/daily_json.js', function (error, response, body) {
-  var data = JSON.parse(body);
+request('https://www.cbr-xml-daily.ru/daily_json.js', (error, response, body) => {
+  const data = JSON.parse(body);
   usdValue = data.Valute.USD.Value;
   eurValue = data.Valute.EUR.Value;
-}); 
+});
 
 async function newUser(ctx) {
   try {
@@ -27,35 +27,48 @@ async function newUser(ctx) {
     console.log(query);
     const newUserId = await collection.findOne(query);
     console.log(newUserId);
-      if (newUserId != null) {
-        console.log('This ID was added before');
-      } else {
-        await collection.insertOne(query);
-        console.log('ID added to DB');
-      };
+    if (newUserId != null) {
+      console.log('This ID was added before');
+    } else {
+      await collection.insertOne(query);
+      console.log('ID added to DB');
     }
-    catch(e) {
-      console.log(e);
-    };
-  };
+  } catch (e) {
+    console.log(e);
+  }
+}
 
 bot.start((ctx) => {
-  ctx.reply('Привет!\nЯ буду отправлять тебе акутальные данные каждый день в 12:00 по Московскому времени.');
-  newUser(ctx).catch(console.dir);});
+  ctx.reply(
+    `Привет, ${ctx.message.from.first_name}!\nЯ буду отправлять тебе акутальные данные каждый день в 12:00 по Московскому времени.`
+  );
+  newUser(ctx).catch(console.dir);
+});
 
-bot.command('now', (ctx) => ctx.reply('Курс доллара на сегодня — ' + usdValue + '\n' + 'Курс евро — ' + eurValue));
+bot.command('now', (ctx) =>
+  ctx.reply(`Курс доллара на сегодня — ${usdValue}\nКурс евро — ${eurValue}`)
+);
 
-// cron.schedule(' 00 12 * * *', () => {
-//   for (const i in chatIdDataBase.id) {
-//     var id = chatIdDataBase.id[i];
-//     bot.telegram.sendMessage(id, 'Курс доллара на сегодня — ' + usdValue + '\n' + 'Курс евро — ' + eurValue)
-//     .catch((e) => 
-//       { chatIdDataBase.id.splice(i,1); 
-//         fs.writeFileSync('./chatIdArray.json', JSON.stringify(chatIdDataBase, null, 2)); 
-//       });
-//     console.log(chatIdDataBase.id);
-//   }
-// });
+cron.schedule(
+  '00 12 * * *',
+  () => {
+    const database = client.db('tbh_currency_bot');
+    const collection = database.collection('users');
+    collection.find().toArray((err, result) => {
+      if (err) throw err;
+      for (let i = 0; i < result.length; i++) {
+        const userId = result[i].id;
+        bot.telegram
+          .sendMessage(userId, `Курс доллара на сегодня — ${usdValue}\nКурс евро — ${eurValue}`)
+          .catch(() => collection.deleteOne({ id: userId }));
+      }
+    });
+  },
+  {
+    scheduled: true,
+    timezone: 'Europe/Moscow',
+  }
+);
 
 bot.launch();
 console.log('Bot has been started...');
